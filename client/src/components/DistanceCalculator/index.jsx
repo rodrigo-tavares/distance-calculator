a6bef968034429b6f8a74447726bdbcfa33f3985
Rodrigo Tavares <rodrigo.tavares@gympass.com>
2024-05-28 00:25:09 -0300

    fix(cors): update policy

--- client/src/components/DistanceCalculator/index.jsx
+++ client/src/components/DistanceCalculator/index.jsx
@@ -24,7 +24,7 @@ const DistanceCalculator = () => {
     setLoading(true);
     try {
       const response = await axios.post(
-        "https://distance-calculator.railway.internal/calculate",
+        "https://distance-calculator.railway.internal:6158/calculate",
         { source, destination }
       );
       if (response.data.success) {
